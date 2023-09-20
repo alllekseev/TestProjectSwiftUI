835aba68621e0b991ae9b9180214aa6bcef3608e
alllekseev <01.wftp@gmail.com>
2023-09-20 12:26:09 +0300

    Init project

---
+++ TestProjectSwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  TestProjectSwiftUI
+//
+//  Created by Олег Алексеев on 14.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestProjectSwiftUI/TestProjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectSwiftUIApp.swift
+//  TestProjectSwiftUI
+//
+//  Created by Олег Алексеев on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
